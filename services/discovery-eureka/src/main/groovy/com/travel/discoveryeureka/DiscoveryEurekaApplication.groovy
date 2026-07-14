780dc2f7750e2a6068c534e4c5aeadd4988a4871
sinee0601 <sinee0601@gmail.com>
2026-07-14 17:29:12 +0900

    first commit

---
+++ services/discovery-eureka/src/main/groovy/com/travel/discoveryeureka/DiscoveryEurekaApplication.groovy
@@ -0,0 +1,13 @@
+package com.travel.discoveryeureka
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DiscoveryEurekaApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DiscoveryEurekaApplication, args)
+    }
+
+}
